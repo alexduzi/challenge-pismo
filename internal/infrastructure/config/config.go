9414bf184e9f38f5ebb7cc1f522d7e5d7d45dd28
Alex Duzi <duzihd@gmail.com>
2026-01-17 19:16:08 -0300

    db connection

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -1,6 +1,8 @@
+// internal/infrastructure/config/config.go
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -8,9 +10,21 @@ import (
 )
 
 type Config struct {
-	AppName string
-	Port    string
-	GinMode string
+	AppName  string
+	Port     string
+	GinMode  string
+	Database DatabaseConfig
+}
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+	MaxConns int
+	MinConns int
 }
 
 var AppConfig *Config
@@ -24,10 +38,21 @@ func LoadConfig() (*Config, error) {
 		port = viper.GetString("PORT")
 	}
 	if port == "" {
-		port = "8080" // Default fallback
+		port = "8080"
 	}
 
-	viper.SetDefault("GIN_MODE", "debug") // debug, release, or test
+	viper.SetDefault("GIN_MODE", "debug")
+	viper.SetDefault("APP_NAME", "api-accounts")
+
+	// Database defaults
+	viper.SetDefault("DB_HOST", "localhost")
+	viper.SetDefault("DB_PORT", "5432")
+	viper.SetDefault("DB_USER", "postgres")
+	viper.SetDefault("DB_PASSWORD", "postgres")
+	viper.SetDefault("DB_NAME", "pismo_challenge")
+	viper.SetDefault("DB_SSLMODE", "disable")
+	viper.SetDefault("DB_MAX_CONNS", 25)
+	viper.SetDefault("DB_MIN_CONNS", 5)
 
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
@@ -38,8 +63,23 @@ func LoadConfig() (*Config, error) {
 	}
 
 	config := &Config{
+		AppName: viper.GetString("APP_NAME"),
 		Port:    port,
 		GinMode: viper.GetString("GIN_MODE"),
+		Database: DatabaseConfig{
+			Host:     viper.GetString("DB_HOST"),
+			Port:     viper.GetString("DB_PORT"),
+			User:     viper.GetString("DB_USER"),
+			Password: viper.GetString("DB_PASSWORD"),
+			DBName:   viper.GetString("DB_NAME"),
+			SSLMode:  viper.GetString("DB_SSLMODE"),
+			MaxConns: viper.GetInt("DB_MAX_CONNS"),
+			MinConns: viper.GetInt("DB_MIN_CONNS"),
+		},
+	}
+
+	if config.Database.Host == "" || config.Database.User == "" {
+		return nil, fmt.Errorf("database configuration is incomplete")
 	}
 
 	AppConfig = config
--- internal/infrastructure/persistence/postgres/db.go
+++ internal/infrastructure/persistence/postgres/db.go
@@ -1,7 +1,46 @@
 package persistence
 
-import "github.com/alexduzi/challengepismo/internal/infrastructure/config"
+import (
+	"fmt"
+	"time"
 
-func InitDb(conf *config.Config) {
+	"github.com/alexduzi/challengepismo/internal/infrastructure/config"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
 
+const driverName = "pgx"
+
+func ConnectDB(conf *config.Config) (*sqlx.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		conf.Database.Host,
+		conf.Database.Port,
+		conf.Database.User,
+		conf.Database.Password,
+		conf.Database.DBName,
+		conf.Database.SSLMode,
+	)
+
+	db, err := sqlx.Connect(driverName, dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	db.SetMaxOpenConns(conf.Database.MaxConns)
+	db.SetMaxIdleConns(conf.Database.MinConns)
+	db.SetConnMaxLifetime(time.Hour)
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	return db, nil
+}
+
+func CloseDB(db *sqlx.DB) error {
+	if db != nil {
+		return db.Close()
+	}
+	return nil
 }
